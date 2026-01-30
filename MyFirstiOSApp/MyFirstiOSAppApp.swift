030ea47ac15919214cb7726e86fa781968d29706
Darshan Kawar <darshan@nevercode.io>
2026-01-30 15:33:48 +0530

    Initial Commit

---
+++ MyFirstiOSApp/MyFirstiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstiOSAppApp.swift
+//  MyFirstiOSApp
+//
+//  Created by Dhs on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
